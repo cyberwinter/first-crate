70580a9c3a0d19930efeb0ad42dd7db9f0525d4d
Releev <r3le@ya.ru>
2018-07-02 23:45:26 +0300

    1
    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let mut f = File::open(config.filename).expect("File not found!");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Failed reading file");
+    println!("With text:\n{}", contents);
+
+
+}
+
+struct Config {
+    query: String,
+    filename: String,
+
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
