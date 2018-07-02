a602171ce131e42c7092819ee4d82e30b88618a4
Releev <r3le@ya.ru>
2018-07-03 01:20:39 +0300

    Dev

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,85 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::env;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn case_sensetive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+
+        )
+
+    }
+    #[test]
+    fn case_insensetive(){
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensetive(query, contents)
+
+        )
+
+}
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+
+
+    }
+    results
+}
+pub fn search_case_insensetive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+
+
+    }
+    results
+}
+
 
 pub fn run(config: Config) -> Result<(), Box<Error>>{
     let mut f = File::open(config.filename)?;//.expect("File not found!");
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
-    println!("With text:\n{}", contents);
+    let results = if config.case_sensetive {
+        println!("sika");
+        search(&config.query, &contents)
+    } else {
+        println!("suka!");
+        search_case_insensetive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
 
 
@@ -15,6 +88,7 @@ pub fn run(config: Config) -> Result<(), Box<Error>>{
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensetive: bool,
 
 }
 
@@ -25,7 +99,8 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensetive = env::var("CASE_INSENSETIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensetive } )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     //println!("{:?}", args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     if let Err(e) = first_crate::run(config) {
-        println!("AppErrro!{}", e);
+
         process::exit(1);
     }
 
